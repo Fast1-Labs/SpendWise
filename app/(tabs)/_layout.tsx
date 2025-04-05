ef6db1b7ccdf69d0586abcda369b147210e968ff
anilyavas <anil.yavas99@gmail.com>
2025-04-05 21:46:41 +0300

    Tab Bar design and configs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,60 @@
-import { Link, Tabs } from 'expo-router';
-
-import { HeaderButton } from '../../components/HeaderButton';
-import { TabBarIcon } from '../../components/TabBarIcon';
+import { FontAwesome } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: 'black',
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: '#FFFFFF',
+          height: 60,
+          marginVertical: 30,
+          marginHorizontal: 20,
+          position: 'absolute',
+          borderRadius: 30,
+          paddingBottom: 10,
+          shadowColor: 'black',
+          shadowOffset: {
+            width: 0,
+            height: 1,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+        },
+        tabBarIconStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          marginTop: 5,
+          marginBottom: 5,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
+          tabBarIcon: ({ color, focused }: any) => (
+            <FontAwesome name="home" size={30} color={focused ? 'black' : 'gray'} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          tabBarIcon: ({ color, focused }: any) => (
+            <FontAwesome name="camera" size={30} color={focused ? 'black' : 'gray'} />
           ),
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color, focused }: any) => (
+            <FontAwesome name="history" size={30} color={focused ? 'black' : 'gray'} />
+          ),
         }}
       />
     </Tabs>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4F46E5', // Indigo-600
+        accent: '#22C55E', // Green-500
+        background: '#F9FAFB', // Gray-50
+        card: '#F3F4F6', // Gray-100
+        textPrimary: '#111827', // Gray-900
+        textSecondary: '#6B7280', // Gray-500
+        error: '#EF4444', // Red-500
+      },
+    },
   },
   plugins: [],
 };
